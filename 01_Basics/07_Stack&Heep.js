402a03b99891fd228beffe27a4e03a3e095bf5f2
Nitish Mandal <138323760+mandalnitish01@users.noreply.github.com>
2023-12-14 13:27:43 +0000

    Stack and Heep Memory

---
+++ 01_Basics/07_Stack&Heep.js
@@ -0,0 +1,13 @@
+// stack(permitive)  heap(non-permitive)
+
+let myname = "nitish"
+let hername = "Arsh"
+
+console.log(myname);
+console.log(hername);
+
+hername = "aman"
+
+console.log(anothername);
+
+// node 07_Stack&Heap_memory.js
