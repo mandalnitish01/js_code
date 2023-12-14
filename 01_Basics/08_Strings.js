d3c429fbeca3d8bfb98433a3c03543bb3dec1c7e
Nitish Mandal <138323760+mandalnitish01@users.noreply.github.com>
2023-12-14 13:27:58 +0000

    Strings

---
+++ 01_Basics/08_Strings.js
@@ -0,0 +1,23 @@
+
+// const name="Nitish"
+// const AnotherName="Babu"
+// const repocount= 50
+
+//basics printing methods of strings
+
+// console.log(name + " " + AnotherName )
+
+
+
+const name="Nitish"
+const AnotherName="Mandal"
+const repocount= 50
+
+// `` :- this is a backtiks string
+console.log(`My name is ${name} ${AnotherName} and my repocount is ${repocount}`);
+
+
+
+// const gamename =  string('nitish')
+
+// console.log(gamename);
