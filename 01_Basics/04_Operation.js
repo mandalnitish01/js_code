cacc9773d1143948047f69c2b29f6ee90fb19af7
Nitish Mandal <138323760+mandalnitish01@users.noreply.github.com>
2023-12-14 13:26:41 +0000

    Operation in js

---
+++ 01_Basics/04_Operation.js
@@ -0,0 +1,12 @@
+let values = 4
+//  let negative = +values output = 4
+ let negative = -values //output = -4
+//  console.log(negative);
+
+//  console.log(6/3)
+//  console.log(6*3)
+//  console.log(6**3)
+//  console.log(6-3)
+//  console.log(6%3)
+//  console.log(6+3) 
+//  node 04_operations.js
