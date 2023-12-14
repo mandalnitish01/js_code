bc3b670c68845d3ba565a88a60405309a9f23987
Nitish Mandal <138323760+mandalnitish01@users.noreply.github.com>
2023-12-14 13:27:00 +0000

    Comparision operators

---
+++ 01_Basics/05_Comparision.js
@@ -0,0 +1,17 @@
+
+// console.log(4>2)
+// console.log(4<2)
+// console.log(4>=2)
+// console.log(4<=2)
+// console.log(4=2)
+// console.log(4==2)
+// console.log(4!=2)
+// console.log(4!=2)
+
+// console.log("4"<2 )
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0) //null>=0
+
+// node 05_Comparision.js
