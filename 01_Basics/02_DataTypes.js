26315d9f7a4335f57397bdc2e16c4733bbc72402
Nitish Mandal <138323760+mandalnitish01@users.noreply.github.com>
2023-12-14 13:25:45 +0000

    DataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //used for Treat all js code as newer version
+
+//alert(3+3) // we are using nodejs, not browser it's run only in browser 
+// because js engin is browser
+
+let name="nitish"
+let age="18"
+let isLoggined= true
+
+
+//number => range 2^53
+
+//bigint
+
+//string =>written in "string"
+
+// boolean => true or false
+
+// null => standalon value 
+// it's not undefined it's null/empty
+
+//undefined => 
+
+// symbol => unique 
+
+//object 
+
+console.log(typeof undefined);
