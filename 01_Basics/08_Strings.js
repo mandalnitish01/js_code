b1b9d586d9fa7f23e2f4443deaa37acfaaecc9fe
Nitish Mandal <138323760+mandalnitish01@users.noreply.github.com>
2023-12-15 04:55:54 +0000

    first commit

--- 01_Basics/08_Strings.js
+++ 01_Basics/08_Strings.js
@@ -18,6 +18,26 @@ console.log(`My name is ${name} ${AnotherName} and my repocount is ${repocount}`
 
 
 
-// const gamename =  string('nitish')
+const gameName = 'nitish_mandal'
 
-// console.log(gamename);
+console.log(gameName)
+console.log(gameName.length)
+console.log(gameName[3])
+console.log(gameName.__proto__)
+console.log(gameName.toUpperCase())
+
+const newstring = gameName.substring(0 , 6)
+console.log(newstring)
+// const anotherString = gameName.slice(-8,4)
+// console.log(anotherString);
+
+ 
+const newStringOne = "   nitish   "
+console.log(newStringOne);
+//trim() is used to hide unappproprite space in your line
+console.log(newStringOne.trim());
+
+//  replace method 
+// (replace(searchValue: { [Symbol.replace](string: string, replaceValue: string): string; })
+const url = "https://google.com/google%20baba"
+console.log(url.replace('%20', '-'));
