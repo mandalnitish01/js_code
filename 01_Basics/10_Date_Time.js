dca8d7dab44c08501e8f36c34936ac1bc6f29701
Nitish Mandal <138323760+mandalnitish01@users.noreply.github.com>
2023-12-15 06:10:20 +0000

    Date and Time

---
+++ 01_Basics/10_Date_Time.js
@@ -0,0 +1,11 @@
+// *******DATE********
+
+
+
+let mydate = new Date()
+console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toLocaleDateString()); //12/15/2023
+// console.log(mydate.toDateString()); //fri dec 15 2023
+// console.log(mydate.toISOString());
+
