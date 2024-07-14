08e5aa193b99add26869a64780cc661f9416006a
Nitish Mandal <138323760+mandalnitish01@users.noreply.github.com>
2024-07-14 15:11:25 +0000

    change

---
+++ JSChallange/day1.js
@@ -0,0 +1,48 @@
+
+// //Task one
+// var num = 34;
+// console.log(num);
+// // output = 34
+
+// // Task 2
+// let name = "nitish";
+// console.log(name);
+// // output = nitish
+
+// // Task 3
+// const float = 34.45;
+// console.log(float);
+// // output = 34.45
+
+
+// // Task 3
+
+// let number = 45;
+// console.log(typeof(number));
+// let string = "name";
+// console.log(typeof(string));
+// let boolean = true;
+// console.log(typeof(boolean));
+
+// let obj = {
+//     name:"nitish",
+//     age: "19",
+//     class:"6th",
+// }
+// console.log(typeof(obj));
+
+// let array = [1,2,3,4,5,6];
+// console.log(typeof(array));
+
+
+// // Task 4
+// let num1 = 34;
+// console.log(num1);
+// num1 = 45;
+// console.log(num1);
+
+// // Task 5
+// const num1 = 34;
+// // num1 = 56; //ReAssignment to constant variable is not possible.
+// console.log(num1)
+
---
+++ JSChallange/day2.js
@@ -0,0 +1,70 @@
+// Task 1
+
+let numberOne = 16;
+let numberTwo = 5;
+
+// Add 2 num
+
+// // one way 
+// console.log(numberOne + numberTwo);
+// // second way 
+// let sum = numberOne + numberTwo;
+// console.log(sum);
+
+
+// subtract 2 number 
+
+
+// // one way 
+// console.log(numberOne - numberTwo);
+// // second way 
+// let sub = numberOne - numberTwo;
+// console.log(sub);
+
+
+ //Multiply 2 num
+
+// // one way 
+// console.log(numberOne * numberTwo);
+// // second way 
+// let Multiply = numberOne * numberTwo;
+// console.log(Multiply);
+
+
+
+//Divide 2 num
+
+// // one way 
+// console.log(numberOne / numberTwo);
+// // second way 
+// let Divide = numberOne / numberTwo;
+// console.log(Divide);
+
+// task 2
+// let d1 = 20;
+// let d2 = 6;
+//  const q = Math.floor(d1/d2);
+ 
+//  console.log(q);
+//  let reminder = d1 - (d2 * q);
+//  console.log(`reminder = ${reminder}`);
+
+
+// Activity 2
+// Task1
+// // += operator 
+// let num = 5;
+// console.log(num)
+// num += 6;
+// console.log(num)
+
+//  Task2
+// // -= operator 
+// let num = 15;
+// console.log(num)
+// num -= 6;
+// console.log(num)
+
+
+
+
