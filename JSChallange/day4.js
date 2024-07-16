ae00f1ab25776695f6d8264fbfdc4f751a330415
Nitish Mandal <138323760+mandalnitish01@users.noreply.github.com>
2024-07-16 05:26:04 +0000

    day4

---
+++ JSChallange/day4.js
@@ -0,0 +1,55 @@
+console.log("day 4!");
+
+// task1
+// write a number to 1 to 10
+// for (let i = 1; i<=10; i++)
+// {
+//     console.log(i);
+// }
+
+
+
+// task2
+// table of 5
+// for(let i = 1; i<=10;i++)
+// {
+//     const result = 5 * i;
+//     console.log(`5 * ${i} = ${result}`);
+// }
+
+
+// task3
+// let i = 1;
+// let sum = 0;
+// while(i <=10)
+// {
+//      sum = sum + i;
+//     i++;
+// }
+// console.log(sum);
+
+
+// task 4
+// let i = 10;
+// while(i>=1)
+// {
+//     console.log(i);
+//     i--;
+// }
+
+// task 5
+// let i = 1;
+// do{
+//     console.log(i);
+//     i++
+// }while(i<=5)
+
+
+// let i=5;
+// let result = 1;
+// do{
+//     result = result * i;
+//     i--;
+// }while(i>=1)
+//     console.log(result);
+
