f0fb3e9e8d30e95662892271c3629a599018d840
Nitish Mandal <138323760+mandalnitish01@users.noreply.github.com>
2024-07-16 04:30:22 +0000

    changes

---
+++ JSChallange/day3.js
@@ -0,0 +1,117 @@
+// activity1
+
+
+// Task1: if-else statement
+
+// // check number is +tive,-tive,zero
+// let num1 = 0;
+
+// if(num1 > 0)
+// {
+//     console.log(`${num1} is Positive!`);
+// }
+// else if(num1 < 0)
+// {
+//     console.log(`${num1} is negative!`);
+// }
+// else{
+//     console.log(`${num1} is zero!`);
+// }
+
+// Task 2
+
+// let age = 18;
+
+// if(age >= 18)
+// {
+//     console.log(`Person is eligible for vote because his/her age is ${age}`);
+// }
+// else{
+//  console.log(`person is not eligible for vote because his/her age is ${age}`);
+// }
+
+// Activity 2
+
+
+// Task3
+// // find largest among three Number
+// let num1 = 3;
+// let num2 = 96;
+// let num3 = 56;
+
+// if(num1 > num2)
+// {
+//     if(num1 > num3)
+//     {
+//         console.log(`${num1} is greater!`);
+//     }
+//     else{
+//         console.log(`${num3} is greater!`);
+//     }
+// }
+// else if(num2 > num1)
+// {
+//     if(num2 > num3)
+//     {
+//         console.log(`${num2} is greater!`);
+//     }
+//     else{
+//         console.log(`${num3} is greater!`);
+// }
+
+// }
+
+
+// Activity 3
+// switch 
+// task4
+
+// let day = 5;
+
+// switch(day)
+// {
+//     case 1:
+//             day = "monday"
+//             console.log(day);
+//             break;
+    
+//         case 2:
+//         day = "tuesday"
+//             console.log(day);
+//             break;
+    
+//         case 3:
+//             day = "wednesday"
+//             console.log(day);
+//             break;
+    
+//         case 4:
+//             day = "thrusday"
+//             console.log(day);
+//             break;
+    
+//         case 5:
+//             day = "friday"
+//             console.log(day);
+//             break;
+        
+//         case 6:
+//             day = "saturday";
+//             console.log(day);
+//             break;
+        
+//         case 7:
+//         day = "sunday"
+//             console.log(day);
+//             break;
+        
+//         default:
+//             console.log("invalid place!");
+// }
+
+
+// ternary operator
+// last task of Assignment 4
+// let num = 2;
+// const result = num %2 == 0 ? "even" : "odd" ;
+// console.log(result);
